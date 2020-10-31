e992335cc4a994eaa83ae9d03e292215f6338f83
iantanxjw <iantanxjw@gmail.com>
2020-10-31 15:00:34 +1100

    added namespace and generated models

---
+++ db/migrate/20201031040014_create_animes.rb
@@ -0,0 +1,11 @@
+class CreateAnimes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :animes do |t|
+      t.string :title
+      t.string :plot
+      t.date :release_date
+
+      t.timestamps
+    end
+  end
+end
