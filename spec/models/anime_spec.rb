16a22e8b3bda8d72100c420bade2f42d18af0629
iantanxjw <iantanxjw@gmail.com>
2020-10-31 15:15:36 +1100

    added factory data and database seed

--- spec/models/anime_spec.rb
+++ spec/models/anime_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Anime, type: :model do
-  it { is_expected.to validate_presence_of(:title) }    it { is_expected.to validate_presence_of(:release_date) }  end
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:title) }    
+    it { is_expected.to validate_presence_of(:release_date) }
+  end
 end
